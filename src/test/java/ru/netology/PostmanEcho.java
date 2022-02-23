71c10ee28819c10a3bb7e855ba6718b266101eac
pippopil <voood234@gmail.com>
2022-02-23 14:56:17 +0300

    Change file

--- src/test/java/ru/netology/PostmanEcho.java
+++ src/test/java/ru/netology/PostmanEcho.java
@@ -16,7 +16,7 @@ class PostmanEcho {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some"))
         ;
     }
 }
