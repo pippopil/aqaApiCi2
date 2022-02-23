b99f9b705539ceb7e4222f59634b6ceeb2c8633b
pippopil <voood234@gmail.com>
2022-02-23 14:44:33 +0300

    Added files

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/d63vcmtyisrl8a1e?svg=true)](https://ci.appveyor.com/project/pippopil/aqaapici-eg7uw)
---
+++ src/test/java/ru/netology/PostmanEcho.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEcho {
+    @Test
+    void ReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
